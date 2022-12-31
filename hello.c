8d0f5c3abeb9f77cb71025bb56b231fd668c293d
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2022-12-31 16:57:22 +0000

    Sat, Dec 31, 2022, 4:57 PM +00:00

--- hello.c
+++ hello.c
@@ -3,6 +3,6 @@
 
 int main()
 {
-    string name = get_string("What is your name? ");
-    printf("Hello, %s\n", name);
+    string name = get_string("What is your name? ");   // Get input from the user.
+    printf("Hello, %s\n", name);   // Print the output on the Computer Screen.
 }
