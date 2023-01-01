040cdd28728b8e3aa98950b4e690b823dda37361
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-01-01 20:11:08 +0000

    Sun, Jan 1, 2022, 8:11 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -10,7 +10,7 @@ int main(void)
     }
     while (height > 8 || height <= 0); // Check the that the number is between 1 to 8. If not it will excute again and again.
 
-    for (int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)  // This is parent loop.
     {
         for (int j = height - i-1; j > 0; j--)
         {
