b5fc63a2cd80e639fd966cf6da9a7c8cc974309c
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-01-01 17:16:31 +0000

    Sun, Jan 1, 2022, 5:16 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -9,11 +9,10 @@ int main()
         height = get_int("Height: ");  // It will ask the user for the Height.
     }
 
-    while (height > 8 || height <= 0); // Check the input Is the input enter
-
+    while (height > 8 || height <= 0); // It will check that the user enter the digits from 1 to 8. If not the statement executed again.
     for (int i = 0; i < height; i++)
     {
-        for (int k = height - i; k > 0; k--)
+        for (int k = height - i; k > 0; k--) // 
         {
              printf(" ");
         }
