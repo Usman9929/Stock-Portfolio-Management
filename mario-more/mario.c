cde93d5b1c61853f65de99826e83142439c4073f
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-01-01 20:04:17 +0000

    Sun, Jan 1, 2022, 8:04 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -8,11 +8,11 @@ int main(void)
     {
         height = get_int("Height:");   // Ask form the user to decide just how tall the pyramids should be.
     }
-    while (height > 8 || height <= 0); // 
+    while (height > 8 || height <= 0); //
 
     for (int i = 0; i < height; i++)
     {
-        for (int j = height - i; j > 0; j--)
+        for (int j = height - i-1; j > 0; j--)
         {
             printf(" ");
         }
