e7d23f7a066047c3fde1071d187f411f144a0055
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-01-01 17:00:23 +0000

    Sun, Jan 1, 2022, 5:00 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -14,8 +14,9 @@ int main()
     for (int i = 0; i < height; i++)
     {
         for (int k = height - i; k > 0; k--)
-        printf(" ");
-
+        {
+         printf(" ");
+        }
         for (int j = 0; j <= i; j++)
         {
             printf("#");
