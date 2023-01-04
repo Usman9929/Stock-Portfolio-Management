c2d95d4a4b1b3a206fe337c418b807e42f412ac2
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-01-04 17:36:22 +0000

    Wed, Jan 4, 2023, 5:36 PM +00:00

--- population/population.c
+++ population/population.c
@@ -10,9 +10,19 @@ int main(void)
     }
     while(start_size < 9);
 
-    // TODO: Prompt for end size
+    int end_size;    //a variable for end_size
+    do
+    {
+         end_size = get_int("End Size: ");
+    }
+    while (end_size < start_size);
 
-    // TODO: Calculate number of years until we reach threshold
+    int years = 0;
+    while (start_size < end_size)
+    {
+        start_size = start_size + (start_size / 3) - (start_size / 4);
+        years++;
+    }
 
-    // TODO: Print number of years
+    printf("Years: %i\n", years);
 }
