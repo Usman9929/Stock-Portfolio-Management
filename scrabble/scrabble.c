75f6c31cfd786b3dfa4676f9f56c7c58daa3ae98
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-01-06 20:09:38 +0000

    Fri, Jan 6, 2023, 8:09 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -40,15 +40,13 @@ int compute_score(string word)
     {
         if(islower(word[i]))
         {
-            POINTS[i] = word[i] - 'a';
+           score += POINTS[word[i] - 'a'];
 
-            score += POINTS[i];
         }
         if(isupper(word[i]))
         {
-            POINTS[i] = word[i] - 'A';
+            score += POINTS[word[i] - 'A'];
 
-            score += POINTS[i];
         }
     }
     return score;
