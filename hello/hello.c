9bf63c380940ec9cd5fd58686b35a61892089a18
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-01-06 19:14:37 +0000

    Fri, Jan 6, 2023, 7:14 PM +00:00

--- hello/hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main()
-{
-    string name = get_string("What is your name? ");   // Get input from the user in this case ask the user name.
-    printf("Hello, %s\n", name);      // Print the output on the Computer Screen in this case show the user name.
-}
