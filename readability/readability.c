9012ed940b8df8ceebb6ec1be15cda752b831cdd
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-01-07 17:48:55 +0000

    Sat, Jan 7, 2023, 5:48 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -13,5 +13,8 @@ int main(void)
     for (int i = 0; i < strlen(text); i++)
     {
         if ((text[i] > 65 && text[i] < 90) || (text[i] > 97 && text[i] < 122))
+        {
+            letters++;
+        }
     }
 }
