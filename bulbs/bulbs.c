898d698ff9c92d7f9f1d0d9a9794c4f3bc8b7780
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-01-11 21:07:55 +0000

    Wed, Jan 11, 2023, 9:07 PM +00:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -31,3 +31,54 @@ int main(void)
 
     }
 }
+
+void text_to_int(string x)
+{
+    for(int i=0 , length = strlen(x); i<length; i++){
+     arr[i]   =  (int) x[i];
+    }
+}
+
+
+
+
+
+
+
+void int_to_binary(int ARR[], int length)
+{   int n =0;
+    int array_length = 0 ;
+    for (int j = 0; j<length; j++){
+     n = arr[j];
+    for (int i = 0; n >=0; i++)
+    {
+        n /= 2;
+        array[array_length] = (char) n%2;
+        array_length++;
+
+        if ( n == 1){
+        array[array_length] = (char) '1';
+        break;
+        }
+        else if (n == 0) {
+            array[array_length] = (char) '0';
+            break;
+        }
+    }
+    }
+
+}
+
+// void print_bulb(int bit)
+// {
+//     if (bit == 0)
+//     {
+//         // Dark emoji
+//         printf("\U000026AB");
+//     }
+//     else if (bit == 1)
+//     {
+//         // Light emoji
+//         printf("\U0001F7E1");
+//     }
+// }
