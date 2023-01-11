bc731b7571db8e82082ebe5d005e7f95ebb9b248
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-01-11 19:45:41 +0000

    Wed, Jan 11, 2023, 7:45 PM +00:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -8,7 +8,7 @@ void print_bulb(int bit);
 
 int main(void)
 {
-    string input = get_string("Message");
+    string input = get_string("Message :");
     for(int i = 0; i <= input[i]; i++)
     {
         int val = input[i];
@@ -22,30 +22,7 @@ int main(void)
         }
     }
 }
-void strToBinary(string s)
-{
-    int n = s.length();
 
-
-    for (int i = 0; i <= n; i++)
-    {
-        // convert each char to
-        // ASCII value
-        int val = int(s[i]);
-
-        // Convert ASCII value to binary
-        string bin = "";
-        while (val > 0)
-        {
-            (val % 2)? bin.push_back('1') :
-                       bin.push_back('0');
-            val /= 2;
-        }
-        reverse(bin.begin(), bin.end());
-
-        cout << bin << " ";
-    }
-}
 void print_bulb(int bit)
 {
     if (bit == 0)
