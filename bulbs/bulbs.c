64de956433f2ee714fb319907788e2ad91547bee
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-01-11 19:56:51 +0000

    Wed, Jan 11, 2023, 7:56 PM +00:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -10,7 +10,7 @@ string toBinary(int n)
 {
     string r;
     while ( n!=0 ){
-        r += ( n % 2 == 0 ? "0" : "1" );
+        r += ( (string)n % 2 == 0 ? "0" : "1" );
         n /= 2;
     }
     return r;
