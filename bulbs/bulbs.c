179c987f16a28cbd35d300cc23d29ed6dfaed52d
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-01-15 17:39:47 +0000

    Sun, Jan 15, 2023, 5:39 PM +00:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -1,30 +1,90 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+
 const int BITS_IN_BYTE = 8;
-// Function Prototype.
+//Functions prototypes
 void print_bulb(char bit);
-void array_reverse(char* input, char* outpu,int size);
 void text_to_binary(string x);
+void reverse_array(char* input, char* output, int size);
 
-// array prototypes
+//array prototypes
 char binary_char[1000];
-char reversed_binary[1000];
+char reversed_binary_char[1000];
 int main(void)
-{
-    //get input from  users
-    string x = get_string("Messege:");
+{   // get input from users
+    string x = get_string("Message:");
 
-    // To calculate the length of the input.
-    int lenght = strlen(x);
+    // to calculate the length of the input
+    int length = strlen(x);
 
-    // text to ASCII values and the binary.
+    //text to ASCII Values and then Binary
     text_to_binary(x);
 
-    // reversing the text arrary after each byte.
-    void array_reverse(binary_char,reversed_binary_char,length * BITS_IN_BYTE);
+    // reversing the text array after each byte
+    reverse_array(binary_char, reversed_binary_char, length * BITS_IN_BYTE);
 
-    // printing hte light bulbs.
+    //printing the light bulbs
     int counter = 0;
-    for (int i =0; i < length *BITS_IN_BYTE)
+    for (int i = 0; i < length * BITS_IN_BYTE;  i++)
+    {
+        print_bulb(reversed_binary_char[i]);
+        counter++;
+        if (counter == BITS_IN_BYTE)
+        {
+            printf("\n");
+            counter = 0;
+        }
+    }
+    printf("\n");
+
+}
+
+// Converting the text value to Binary
+void text_to_binary(string x)
+{
+    int binary_index = 0;
+    for (int i = 0, length = strlen(x); i < length; i++)
+    {
+        int ascii = (int)x[i];
+        for (int j = 0; j < BITS_IN_BYTE; j++)
+        {
+            if (ascii % 2 == 0)
+            {
+                binary_char[binary_index] = '0';
+            }
+            else
+            {
+                binary_char[binary_index] = '1';
+            }
+            ascii /= 2;
+            binary_index++;
+        }
+    }
+}
+
+void print_bulb(char bit)
+{
+    if (bit == '0')
+    {
+        // Dark emoji
+        printf("\U000026AB");
+    }
+    else if (bit == '1')
+    {
+        // Light emoji
+        printf("\U0001F7E1");
+    }
+}
+
+// special function to break the charachters in the binary_array
+//bytes and then store them in revrse order to get the expected output
+void reverse_array(char* input, char* output, int size) {
+    for (int i = 0; i < size; i += 8) {
+        int start = i;
+        int end = i + 8 - 1;
+        for (int j = end; j >= start; j--) {
+            output[i + (end - j)] = input[j];
+        }
+    }
 }
