1632785c7fed71d7a7ab4d5544a9f4d9c9b76c92
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-01-15 18:50:59 +0000

    Sun, Jan 15, 2023, 6:50 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
@@ -7,6 +8,14 @@ int main(int argc, string argv[])
     {
         printf("Usage: ./caesar key");
         return 1;
+    }
+    for(int i = 0; i < strlen(argv[1]); i++)
+    {
+        if(!isdigit(argv[1][i]))
+        {
+            printf("Usage: ./caesar key");
+            return 1;
+        }
     }
 
 }
