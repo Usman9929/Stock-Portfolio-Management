766e98376d84b702a965acb1979bcd0e279e7912
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-01-21 17:20:11 +0000

    Sat, Jan 21, 2023, 5:20 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    // Check that there is one command-line argument.
+    if(argc != 2)
+    {
+        printf("Usage: ./caesar Key\n");
+        return 1;
+    }
+    else
+    {
+        printf("Success!\n");
+    }
 
 }
