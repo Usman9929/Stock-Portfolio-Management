6e6c3111794098e5a72bfdc1df90c99f971d3ef5
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-01-21 18:08:49 +0000

    Sat, Jan 21, 2023, 6:08 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -34,19 +34,19 @@ int main(int argc, string argv[])
         printf("cipertext: ");
 
         //Obtain Cipertext.
-        for(int i = 0; i < strlen(plaintext); i++)
+        for(int j = 0; j < strlen(plaintext); j++)
         {
-            if(isupper(plaintext[i]))
+            if(isupper(plaintext[j]))
             {
-                printf("%c", (((plaintext[i] - 65) + k) % 26) + 65);
+                printf("%c", (plaintext[j] - 65 + k) % 26 + 65);
             }
-            else if(islower(plaintext[i]))
+            else if(islower(plaintext[j]))
             {
-                printf("%c", (((plaintext[i] - 97) + k) % 26) + 97);
+                printf("%c", (plaintext[j] - 97 + k) % 26 + 97);
             }
             else
             {
-                printf("%c", plaintext[i]);
+                printf("%c", plaintext[j]);
             }
         }
         printf("\n");
