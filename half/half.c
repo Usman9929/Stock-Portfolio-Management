9f993fbc0a308724253a66921b0cc63d533a6121
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-05-30 09:24:35 +0000

    Tue, May 30, 2023, 2:24 AM -07:00

--- half/half.c
+++ half/half.c
@@ -15,7 +15,7 @@ int main(void)
     printf("You will owe $%.2f each!\n", half(bill_amount, tax_percent, tip_percent));
 }
 
-// TODO: Complete the 
+// TODO: Complete the
 float half(float bill, float tax, int tip)
 {
     return 0.0;
