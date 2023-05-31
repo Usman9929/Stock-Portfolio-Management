9997b21078d3e6b1b37ef7a28dd9b344c3862a65
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-05-31 09:12:55 +0000

    Wed, May 31, 2023, 2:12 AM -07:00

--- practice/practice.c
+++ practice/practice.c
@@ -2,15 +2,5 @@
 #include <cs50.h>
 int main()
 {
-    int x = get_int("Enter value for x:");
-    int y = get_int("Enter value for y:");
-
-    if(x<y)
-    {
-        printf("x is less than y\n");
-    }
-    else
-    {
-        printf("Y is less than X\n");
-    }
+    
 }
