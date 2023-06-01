8bc98d7b8ecde3ab07f7a1cf543070201f8d0c72
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-06-01 10:17:57 +0000

    Thu, Jun 1, 2023, 3:17 AM -07:00

--- half/half.c
+++ half/half.c
@@ -18,6 +18,6 @@ int main(void)
 // TODO: Complete the function
 float half(float bill, float tax, int tip)
 {
-    float result = (bill + tax / tip);
+    float result = ((bill + tax) / tip);
     return result;
 }
