6ed44349a182ee007235fdac9b44bbf326bf5b29
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-06-01 10:02:51 +0000

    Thu, Jun 1, 2023, 3:02 AM -07:00

--- practice/practice.c
+++ practice/practice.c
@@ -1,32 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
-int get_size(void);
-void print_grid(int size);
+
 int main()
 {
-   int n = get_size();
-    print_grid(n);
-}
-
-int get_size(void)
-{
-    int n;
-    do
-    {
-        n = get_int("Size :");
-    }
-    while(n < 1);
-    return n;
-}
-
-void print_grid(int size)
-{
-    for(int i = 0; i < size; i++)
-    {
-        for(int j = 0; j < size; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
-    }
+    int x = (true) ? 5 : 6;
 }
