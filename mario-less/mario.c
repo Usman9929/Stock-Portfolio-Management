a2cda8013560272d6be7bdb3efb6e9cea2779d19
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-06-05 09:54:00 +0000

    Mon, Jun 5, 2023, 2:54 AM -07:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -10,15 +10,13 @@ int main(void)
  }
  while(n <= 0 || n > 8);
 
-    for(int i = 1; i < n; i++)
-    {
-         for (int i = 0; i < n; i++)
+     for (int i = 0; i < n; i++)
   {
       for (int j = 0; j < n; j++)
       {
+        
           printf("#");
       }
       printf("\n");
   }
-    }
 }
