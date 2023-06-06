09b861bf01988d10c464454f491c920079126f75
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-06-06 10:11:54 +0000

    Tue, Jun 6, 2023, 3:11 AM -07:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -4,13 +4,14 @@
 int main(void)
 {
     int height;
+    int i;
     do
     {
         height = get_int("Height: ");
     }
     while(height < 0 || height >= 8);
 
-    for(int i = 0; i < height; i++)
+    for( i = 0; i < height; i++)
     {
         for(int k = height-i-1; k > 0; k--)
         {
@@ -20,8 +21,8 @@ int main(void)
         {
             printf("#");
         }
-        printf("..");
-        for(int b = 0; b < height; b++)
+        printf("  ");
+        for(int b = 0; b <= i; b++)
         {
             printf("#");
         }
