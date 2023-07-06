88e5c18cca6d5ca537d70f825e58a3af3d748f4a
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-06 09:51:56 +0000

    Thu, Jul 6, 2023, 2:51 AM -07:00

--- practice/practice.c
+++ practice/practice.c
@@ -1,39 +1,7 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 
-int binarySearch(int arr[], int size, int key)
-{
-    int start = 0;
-    int end = start-1;
-
-    int mid( (int start) + (int end))/2;
-
-    while(start <= end)
-    {
-        if(arr[mid] == key)
-        {
-            return mid;
-        }
-
-        if(key > mid[arr])
-        {
-            start = mid + 1;
-        }
-        else
-        {
-            end = end - 1;
-        }
-        mid = (start + end)/2;
-
-    }
-    return -1;
-}
 int main()
 {
-    int even[6] = {2,4,6,8,10,12};
-    int odd[6] = {1,3,5,7,9,11};
-
-    int index = binarySearch(even, 6, 10);
-
-    printf("index of 10 is ,%i", index);
+    printf("hello, word");
 }
