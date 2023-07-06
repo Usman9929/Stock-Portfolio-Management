8b8c0c31566fa207d6759d3d3b6283e199477953
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-06 11:08:39 +0000

    Thu, Jul 6, 2023, 4:08 AM -07:00

--- practice/practice.c
+++ practice/practice.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int get_negative_int(void);
+
 int main()
 {
-    for(int i = 0; i <= 3; i++)
+    int i = get_negative_int();
+    printf("%i\n", i);
+}
+
+int get_negative_int()
+{
+    int n;
+    do
     {
-        printf("#\n");
+        n = get_int("Negative interger: ");
     }
+    while(n < 0);
+    return n;
 }
