e8171f398ab5b3aae3d2f7c9eb8f7de56436483e
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-08 12:47:07 +0000

    Sat, Jul 8, 2023, 5:47 AM -07:00

--- practice/practice.c
+++ practice/practice.c
@@ -5,5 +5,11 @@
 
 int main()
 {
-   s
+   string s = get_string("Before: ");
+   printf("After: ");
+   for(int i = 0; i < strlen(s); i++)
+   {
+    printf("%c", toupper(s[i]));
+   }
+   printf("\n");
 }
