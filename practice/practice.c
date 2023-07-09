976aa27f85b22893895459b0611e0ebc53d47693
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-09 18:26:43 +0000

    Sun, Jul 9, 2023, 11:26 AM -07:00

--- practice/practice.c
+++ practice/practice.c
@@ -2,11 +2,12 @@
 
 int main()
 {
-   int foo[4] = {30,40,59,30,23};
-   int bar[4];
-   for(int j = 0; j < 5; j++)
+   int foo[5] = {30,40,59,30,23};
+   int bar[5];
+   int j;
+   for(j = 0; j < 5; j++)
    {
       bar[j] = foo[j];
    }
-   printf("%i", bar[j]);
+   printf("%i\n", bar[j]);
 }
