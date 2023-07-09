ae212ca7224525b56ab2d611c95fe9a71634f51b
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-09 19:08:21 +0000

    Sun, Jul 9, 2023, 12:08 PM -07:00

--- hours/hours.c
+++ hours/hours.c
@@ -27,5 +27,8 @@ int main(void)
 // TODO: complete the calc_hours function
 float calc_hours(int hours[], int weeks, char output)
 {
-    
+    for(int j = 0 j < week; j++)
+    {
+        hour[j]
+    }
 }
