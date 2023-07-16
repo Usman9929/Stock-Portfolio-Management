d43d1644fc2e1c3cf2f14c1c2d673518ed6b26db
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-16 18:54:30 +0000

    Sun, Jul 16, 2023, 11:54 AM -07:00

--- practice/practice.c
+++ practice/practice.c
@@ -1,47 +1,44 @@
 #include <cs50.h>
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-
-int obtain_cipertext(string text);
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 int main(int argc, string argv[])
 {
-    if (argc != 2)
+    if(argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
 
-    for (int i = 0; i < strlen(argv[1]); i++)
-    {
-        if (!isdigit(argv[1][i]))
+    for(int i = 0; i < strlen(argv[1]); i++)
         {
-            printf("Usage: ./caesar key\n");
-            return 1;
+            if(!isdigit(argv[1][i]))
+            {
+                printf("Usage: ./caesar key\n");
+                return 1;
+            }
         }
-    }
-    string plaintext = get_string("Plaintext: ");
-    printf("ciphertext: ");
-}
-int obtain_cipertext(string text);
-{
-    int k = atoi(argv[1]);
-    for (int j = 0; j < strlen(plaintext); j++)
-    {
-        if (isupper(plaintext[j]))
+        int k = atoi(argv[1]);
+        string plaintext = get_string("Plaintext: ");
+        printf("ciphertext: ");
+
+        for(int j = 0; j < strlen(plaintext); j++)
         {
-            printf("%c", plaintext[j] - 65 + k % 26 + 65);
+            if(isupper(plaintext[j]))
+            {
+                printf("%c", plaintext[j] - 65 + k % 26 + 65);
+            }
+            else if(islower(plaintext[j]))
+            {
+                printf("%c", plaintext[j] - 97 + k % 26 + 97);
+            }
+            else
+            {
+                printf("%c", plaintext[j]);
+            }
         }
-        else if (islower(plaintext[j]))
-        {
-            printf("%c", plaintext[j] - 97 + k % 26 + 97);
-        }
-        else
-        {
-            printf("%c", plaintext[j]);
-        }
-    }
-    printf("\n");
+        printf("\n");
+
 }
