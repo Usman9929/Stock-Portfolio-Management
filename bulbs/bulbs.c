ad0319b79d63d62c31d88cdb24cc0dcec48400bd
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-16 09:41:24 +0000

    Sun, Jul 16, 2023, 2:41 AM -07:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
 const int BITS_IN_BYTE = 8;
 
@@ -13,22 +13,24 @@ int main(void)
     string text = get_string("Messege:");
     int decimal = 0;
     int binary[8];
-    for(int i = 0; i < strlen(text); i++)
+    for (int i = 0; i < strlen(text); i++)
     {
-        if(isupper(text[i]) || islower(text[i]))
+        if (isupper(text[i]) || islower(text[i]))
         {
             decimal = text[i];
-            while(decimal > 0)
+            while (decimal > 0)
             {
                 binary[i] = decimal % 2;
                 decimal = decimal / 2;
                 i++;
             }
-
+            for (int j = i - 1; j >= 0; j--)
+            {
+                printf("%d", binary[j]);
+            }
         }
     }
     printf("%i\n", decimal);
-
 }
 
 void print_bulb(int bit)
