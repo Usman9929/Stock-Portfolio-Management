248e383469a6c375d151685ed2018ff8a4536b27
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-16 09:55:45 +0000

    Sun, Jul 16, 2023, 2:55 AM -07:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -26,11 +26,10 @@ int main(void)
             }
             for (int j = i - 1; j >= 0; j--)
             {
-                printf("%d", binary[j]);
+                
             }
         }
     }
-    printf("%i\n", decimal);
 }
 
 void print_bulb(int bit)
