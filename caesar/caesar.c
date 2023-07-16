5a8aa4db1c4bb3bbc4364fd7f8a9ce3448ddc50c
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-16 18:22:37 +0000

    Sun, Jul 16, 2023, 11:22 AM -07:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,20 +1,49 @@
 #include <cs50.h>
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, string argv[])
 {
-    if(argc != 2)
+    // Check that there is one command-line argument.
+    if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
-    for(int i = 0; i < strlen(argv[1]); i++)
+
+    // Check that the input is a digit.
+    for (int i = 0; i < strlen(argv[1]); i++)
     {
-        if(!isdigit(argv[1][i]))
+        if (!isdigit(argv[1][i]))
         {
-           printf("Usage: ./caesar key\n");
+            printf("Usage: ./caesar key\n");
+            return 1;
         }
     }
+    // Convert key to integer.
+    int k = atoi(argv[1]);
+
+    // Get Plaintext from the user.
+    string plaintext = get_string("Plaintext: ");
+    printf("ciphertext: ");
+
+    // Obtain Cipertext.
+    for (int j = 0; j < strlen(plaintext); j++)
+    {
+        if (isupper(plaintext[j]))
+        {
+            printf("%c", (plaintext[j] - 65 + k) % 26 + 65);
+        }
+        else if (islower(plaintext[j]))
+        {
+            printf("%c", (plaintext[j] - 97 + k) % 26 + 97);
+        }
+        else
+        {
+            printf("%c", plaintext[j]);
+        }
+    }
+    printf("\n");
 }
