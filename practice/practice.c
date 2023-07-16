882f16d229ed4774f9a6ffde50c1425f88fd4929
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-16 18:53:00 +0000

    Sun, Jul 16, 2023, 11:53 AM -07:00

--- practice/practice.c
+++ practice/practice.c
@@ -1,48 +1,47 @@
 #include <cs50.h>
-#include <string.h>
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int obtain_cipertext(string text);
 
 int main(int argc, string argv[])
 {
-    if(argc != 2)
+    if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
 
-    for(int i = 0; i < strlen(argv[1]); i++)
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if (!isdigit(argv[1][i]))
         {
-            if(!isdigit(argv[1][i]))
-            {
-                printf("Usage: ./caesar key\n");
-                return 1;
-            }
+            printf("Usage: ./caesar key\n");
+            return 1;
         }
-        string plaintext = get_string("Plaintext: ");
-        printf("ciphertext: ");
-
+    }
+    string plaintext = get_string("Plaintext: ");
+    printf("ciphertext: ");
 }
 int obtain_cipertext(string text);
 {
-
-        for(int j = 0; j < strlen(plaintext); j++)
+    int k = atoi(argv[1]);
+    for (int j = 0; j < strlen(plaintext); j++)
+    {
+        if (isupper(plaintext[j]))
         {
-            if(isupper(plaintext[j]))
-            {
-                printf("%c", plaintext[j] - 65 + k % 26 + 65);
-            }
-            else if(islower(plaintext[j]))
-            {
-                printf("%c", plaintext[j] - 97 + k % 26 + 97);
-            }
-            else
-            {
-                printf("%c", plaintext[j]);
-            }
+            printf("%c", plaintext[j] - 65 + k % 26 + 65);
         }
-        printf("\n");
+        else if (islower(plaintext[j]))
+        {
+            printf("%c", plaintext[j] - 97 + k % 26 + 97);
+        }
+        else
+        {
+            printf("%c", plaintext[j]);
+        }
+    }
+    printf("\n");
 }
