8df53589913f1738f55cdba14d14c5dcb8158070
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-16 09:59:31 +0000

    Sun, Jul 16, 2023, 2:59 AM -07:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -1,28 +1,99 @@
 #include <cs50.h>
-#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
 const int BITS_IN_BYTE = 8;
+// Functions prototypes
+void print_bulb(char bit);
+void text_to_binary(string x);
+void reverse_array(char *input, char *output, int size);
 
-void print_bulb(int bit);
-
+// array prototypes
+char binary_char[1000];
+char reversed_binary_char[1000];
 int main(void)
 {
-    // TODO
-    
+    // get input from users
+    string x = get_string("Message:");
+
+    // to calculate the length of the input
+    int length = strlen(x);
+
+    // text to ASCII Values and then Binary
+    text_to_binary(x);
+
+    // reversing the text array after each byte
+    reverse_array(binary_char, reversed_binary_char, length * BITS_IN_BYTE);
+
+    // printing the light bulbs
+    int counter = 0;
+    int i = 0;
+    while (i < length * BITS_IN_BYTE)
+    {
+        print_bulb(reversed_binary_char[i]);
+        counter++;
+        if (counter == BITS_IN_BYTE)
+        {
+            printf("\n");
+            counter = 0;
+        }
+        i++;
+    }
+    printf("\n");
 }
 
-void print_bulb(int bit)
+// Converting the text value to Binary
+void text_to_binary(string x)
 {
-    if (bit == 0)
+    int binary_index = 0;
+    int i = 0, length = strlen(x);
+    while (i < length)
+    {
+        int ascii = (int) x[i];
+        for (int j = 0; j < BITS_IN_BYTE; j++)
+        {
+            if (ascii % 2 == 0)
+            {
+                binary_char[binary_index] = '0';
+            }
+            else
+            {
+                binary_char[binary_index] = '1';
+            }
+            ascii /= 2;
+            binary_index++;
+        }
+        i++;
+    }
+}
+
+void print_bulb(char bit)
+{
+    if (bit == '0')
     {
         // Dark emoji
         printf("\U000026AB");
     }
-    else if (bit == 1)
+    else if (bit == '1')
     {
         // Light emoji
         printf("\U0001F7E1");
+    }
+}
+
+// special function to break the charachters in the binary_array
+// bytes and then store them in revrse order to get the expected output
+void reverse_array(char *input, char *output, int size)
+{
+    int i = 0;
+    while (i < size)
+    {
+        int begin = i;
+        int end = i + 8 - 1;
+        for (int j = end; j >= begin; j--)
+        {
+            output[i + (end - j)] = input[j];
+        }
+        i += 8;
     }
 }
