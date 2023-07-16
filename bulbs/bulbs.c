f4f0b663705c411e058aafd40b5431451f21fb0e
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-16 09:58:14 +0000

    Sun, Jul 16, 2023, 2:58 AM -07:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -10,26 +10,7 @@ void print_bulb(int bit);
 int main(void)
 {
     // TODO
-    string text = get_string("Messege:");
-    int decimal = 0;
-    int binary[8];
-    for (int i = 0; i < strlen(text); i++)
-    {
-        if (isupper(text[i]) || islower(text[i]))
-        {
-            decimal = text[i];
-            while (decimal > 0)
-            {
-                binary[i] = decimal % 2;
-                decimal = decimal / 2;
-                i++;
-            }
-            for (int j = i - 1; j >= 0; j--)
-            {
-                
-            }
-        }
-    }
+    
 }
 
 void print_bulb(int bit)
