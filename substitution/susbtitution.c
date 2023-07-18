8d88e5cfa5bb00c5af1c48ca8b9538496bc633bd
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-18 09:19:17 +0000

    Tue, Jul 18, 2023, 2:19 AM -07:00

---
+++ substitution/susbtitution.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, string argv[])
+{
+    if(argc != 2)
+    {
+        printf("Usage: ./substitution key");
+        return 1;
+    }
+    while(strlen(argv[]) < 26)
+    {
+        printf("Key must contain 26 characters.");
+        return 1;
+    }
+    printf("plaintext: ");
+}
