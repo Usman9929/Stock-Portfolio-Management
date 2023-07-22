cff22066e2afb40a58902e310d36f56ef0ca32ea
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-22 10:08:30 +0000

    Sat, Jul 22, 2023, 3:08 AM -07:00

--- practice/practice.c
+++ practice/practice.c
@@ -1,44 +1,22 @@
 #include <cs50.h>
-#include <string.h>
 #include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
 
-int main(int argc, string argv[])
+int factorial(int fact);
+
+int main()
 {
-    if(argc != 2)
+    int fact1 = get_int("Enter value to find out his factorial:");
+    int result = factorial(fact1);
+
+    printf("%i",result);
+    printf("\n");
+}
+
+int factorial(int fact)
+{
+    for(int i = fact; i > 0; i--)
     {
-        printf("Usage: ./caesar key\n");
-        return 1;
+        fact = fact * i;
     }
-
-    for(int i = 0; i < strlen(argv[1]); i++)
-        {
-            if(!isdigit(argv[1][i]))
-            {
-                printf("Usage: ./caesar key\n");
-                return 1;
-            }
-        }
-        int k = atoi(argv[1]);
-        string plaintext = get_string("Plaintext: ");
-        printf("ciphertext: ");
-
-        for(int j = 0; j < strlen(plaintext); j++)
-        {
-            if(isupper(plaintext[j]))
-            {
-                printf("%c", plaintext[j] - 65 + k % 26 + 65);
-            }
-            else if(islower(plaintext[j]))
-            {
-                printf("%c", plaintext[j] - 97 + k % 26 + 97);
-            }
-            else
-            {
-                printf("%c", plaintext[j]);
-            }
-        }
-        printf("\n");
-
+    return fact;
 }
