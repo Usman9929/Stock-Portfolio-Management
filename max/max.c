baaf50f7efdaef6ace69576900dc071ff52ae6d2
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-28 19:46:37 +0000

    Fri, Jul 28, 2023, 12:46 PM -07:00

--- max/max.c
+++ max/max.c
@@ -27,8 +27,9 @@ int main(void)
 // TODO: return the max value
 int max(int array[], int n)
 {
+    int i;
     int j = 0;
-    for(int i = 0; i < n; i++)
+    for(i = 0; i <= n; i++)
     {
         if(array[i] > array[i+1])
         {
