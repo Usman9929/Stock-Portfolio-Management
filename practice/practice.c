8956dad4d60b9210474224ddcd3c2403031ae4ca
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-28 12:40:53 +0000

    Fri, Jul 28, 2023, 5:40 AM -07:00

--- practice/practice.c
+++ practice/practice.c
@@ -5,16 +5,22 @@ int main()
 {
     int i, u, t, array[4] = {4, 19, 1, 3};
     for (u = 3; u > 0; u--)
+    {
         for (i = 0; i < u; i++)
+        {
+
             if (array[i] > array[i + 1])
             {
                 t = array[i];
                 array[i] = array[i + 1];
                 array[i + 1] = t;
             }
+        }
+    }
         printf("Sorted Values \n");
-        for(int i = 0; i <= 3; i++)
+        for(i = 0; i <= 3; i++)
         {
-            printf(array[i], "\n");
+            printf("%i", array[i]);
+            printf("\n");
         }
 }
