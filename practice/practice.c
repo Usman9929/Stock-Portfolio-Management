d9ec04b8cdd2c2c9b56f7e9a122e1ad116600c77
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-07-28 12:37:41 +0000

    Fri, Jul 28, 2023, 5:37 AM -07:00

--- practice/practice.c
+++ practice/practice.c
@@ -1,22 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int factorial(int fact);
-
 int main()
 {
-    int fact1 = get_int("Enter value to find out his factorial:");
-    int result = factorial(fact1);
-
-    printf("%i",result);
-    printf("\n");
-}
-
-int factorial(int n)
-{
-    if(n == 1)
-    {
-        return 1;
-    }
-    return n * factorial(n - 1);
+    int i, u, t, array[4] = {4, 19, 1, 3};
+    for (u = 3; u > 0; u--)
+        for (i = 0; i < u; i++)
+            if (array[i] > array[i + 1])
+            {
+                t = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = t;
+            }
+        printf("Sorted Values \n");
+        for(int i = 0; i <= 3; i++)
+        {
+            printf(array[i], "\n");
+        }
 }
