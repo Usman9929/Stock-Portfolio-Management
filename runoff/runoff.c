594a83b30e8e96628e0ff1264704cd11e79f0cdb
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-08-03 10:31:56 +0000

    Thu, Aug 3, 2023, 3:31 AM -07:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -161,7 +161,15 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    
+    int maximum = (voter_count / 2) + 1;
+    for(int i = 0; i < candidates_count; i++)
+    {
+        if(candidates[i].votes >= maximum)
+        {
+            printf("%s", candidates[i].name);
+            return true;
+        }
+    }
     return false;
 }
 
