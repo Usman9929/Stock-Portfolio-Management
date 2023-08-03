fdf5fd5d00934382ac349b601e26de951b786f5e
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-08-03 10:52:03 +0000

    Thu, Aug 3, 2023, 3:52 AM -07:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -214,6 +214,12 @@ bool is_tie(int min)
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
-    
+    for(int i = 0; i < candidates_count; i++)
+    {
+        if(!candidates[i].eliminated && candidates[i].votes == min)
+        {
+            return true;
+        }
+    }
     return;
 }
