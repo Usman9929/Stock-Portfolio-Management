8643caac55e7849d59962cf75eb1cba3e75fdaf8
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-08-03 06:47:38 +0000

    Wed, Aug 2, 2023, 11:47 PM -07:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -67,12 +67,17 @@ int main(int argc, string argv[])
 bool vote(string name)
 {
     // TODO
-    /* candidate candidates1[3];
+    candidate candidates1[3];
     candidates1[0].name = "Alice";
     candidates1[1].name = "Bob";
-    candidates1[2].name = "Charlie"; */
+    candidates1[2].name = "Charlie";
+    for(int i = 0; i < MAX; i++)
+    {
+        if(strcmp(candidates[i].name,name) == 0)
+        return true;
+    }
 
-    return true;
+    return false;
 }
 
 // Print the winner (or winners) of the election
