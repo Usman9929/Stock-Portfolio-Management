574a0d63082fe0265ffa8ca83bf0d27bc83cdcac
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-08-09 09:29:26 +0000

    Wed, Aug 9, 2023, 2:29 AM -07:00

--- practice/practice.c
+++ practice/practice.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <cs50.h>
 
+int factorial(int n);
 int main()
 {
-    char *s = "HI!";
-    printf("%c\n",*s);
-    printf("%c\n",*(s+1));
-    printf("%c\n",*(s+2));
+    int fact = get_int("Enter factorial of a number:");
+    printf("%i\n",factorial(fact));
+}
+int factorial(int n)
+{
+    if(n == 1)
+    {
+        return 1;
+    }
+    return n * factorial(n-1);
 }
