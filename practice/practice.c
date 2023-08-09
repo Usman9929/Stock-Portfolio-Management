9cc27dca1ed1e47af84e2edd64e4041d1c349a4c
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-08-09 11:06:24 +0000

    Wed, Aug 9, 2023, 4:06 AM -07:00

--- practice/practice.c
+++ practice/practice.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-    int *x  = malloc(3 *sizeof(int));
-    if(x == NULL)
-    {
-        return 1;
-    }
-    x[0] = 72;
-    x[1] = 73;
-    x[2] = 74;
-    return 0;
+    int x;
+    printf("x:");
+    scanf("%i", &x);
+    printf("x %i\n", x);
 }
