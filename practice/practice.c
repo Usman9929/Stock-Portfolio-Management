3ad7e18d195f3cfcacf962b97aa1f8e1b6ceb512
Usman9929 <121403144+Usman9929@users.noreply.github.com>
2023-08-09 11:21:46 +0000

    Wed, Aug 9, 2023, 4:21 AM -07:00

--- practice/practice.c
+++ practice/practice.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <cs50.>
+#include <cs50.h>
 #include <string.h>
 
 int main()
 {
     FILE *file = fopen("phonebook.csv", "a");
     string name = get_string("Name:");
-    string number = get_sting("Number:");
+    string number = get_string("Number:");
     fprintf(file, "%s, %s\n", name, number);
 
     fclose(file);
