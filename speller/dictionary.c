70c62a110e3862f3d92380d75440a2ed4e1b995a
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-21 20:47:25 +0000

    Thu, Sep 21, 2023, 1:47 PM -07:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -2,11 +2,10 @@
 
 #include <ctype.h>
 #include <stdbool.h>
-#include <strings.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
+#include <strings.h>
 
 #include "dictionary.h"
 
@@ -15,8 +14,7 @@ typedef struct node
 {
     char word[LENGTH + 1];
     struct node *next;
-}
-node;
+} node;
 
 // TODO: Choose number of buckets in hash table
 const unsigned int N = 26;
@@ -35,9 +33,9 @@ bool check(const char *word)
     hash_value = hash(word);
     node *cursor = table[hash_value];
 
-    while(cursor != 0)
+    while (cursor != 0)
     {
-        if (strcmp(word, cursor->word)== 0)
+        if (strcmp(word, cursor->word) == 0)
         {
             return true;
         }
@@ -51,7 +49,7 @@ unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
     unsigned long total = 0;
-    for(int i = 0; i < strlen(word); i++)
+    for (int i = 0; i < strlen(word); i++)
     {
         total += tolower(word[i]);
     }
@@ -61,26 +59,26 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-   FILE *file = fopen(dictionary, "r");
+    FILE *file = fopen(dictionary, "r");
     if (file == NULL)
     {
         printf("Unable to open %s\n", dictionary);
         return false;
     }
     char word[LENGTH + 1];
-    while(fscanf(file, "%s", word) != EOF)
+    while (fscanf(file, "%s", word) != EOF)
     {
         node *n = malloc(sizeof(node));
 
-    if (n == NULL)
-    {
-        return false;
-    }
-    strcpy(n->word, word);
-    hash_value = hash(word);
-    n->next = table[hash_value];
-    table[hash_value] = n;
-    word_count++;
+        if (n == NULL)
+        {
+            return false;
+        }
+        strcpy(n->word, word);
+        hash_value = hash(word);
+        n->next = table[hash_value];
+        table[hash_value] = n;
+        word_count++;
     }
     fclose(file);
     return true;
@@ -99,10 +97,10 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    for(int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
         node *cursor = table[i];
-        while(cursor)
+        while (cursor)
         {
             node *temp = cursor;
             cursor = cursor->next;
