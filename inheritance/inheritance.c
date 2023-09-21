9e85267ccf23a351f600428d6566e3d1aaace95c
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-21 20:04:03 +0000

    Thu, Sep 21, 2023, 1:04 PM -07:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -49,12 +49,12 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        p->parents[0] = parent0;
-        p->parents[1] = parent1;
+        p->parent0 = parent0;
+        p->parent1 = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        p->alleles[0] = parent0->alleles[rand() % 2];
-        p->alleles[1] = parent1->alleles[rand() % 2];
+        p->alleles[0] = p->parents[0]->alleles[rand() % 2];
+        p->alleles[1] = p->parents[1]->alleles[rand() % 2];
     }
 
     // If there are no generations left to create
@@ -65,12 +65,12 @@ person *create_family(int generations)
         p->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
-        p->alleles[0] - random_allele();
-        p->alleles[1] - random_allele();
+        p->alleles[0] = random_allele();
+        p->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
-    return NULL;
+    return p;
 }
 
 // Free `p` and all ancestors of `p`.
