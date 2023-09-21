0d0d9237da1b1eb9afcb1e4059de17855db8abdc
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-21 09:18:24 +0000

    Thu, Sep 21, 2023, 2:18 AM -07:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,5 +3,15 @@
 
 int main(int argc, char *argv[])
 {
+    // Check for correct usage
+    // Print error messege if the program is not given the correct number of argument.
+    // Open the ferosonic image file specified in the command line arguments.
+    // Initilize variable.
+    // Flag to keep track of weather a JPEG has been found.
+    // Counter of the number of JPEGs found.
+    // Buffer to store a block of data from the forensic image(Notes)
+    // Array to store the file name of the current JPEG
+    // Pointer to the current JPEG File.
+    
 
 }
