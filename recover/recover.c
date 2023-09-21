1ffcdcd8faa5f4c084899404534bad2060d7a9f9
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-21 09:42:29 +0000

    Thu, Sep 21, 2023, 2:42 AM -07:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 #define BLOCK_SIZE 512 // from notes
 
@@ -14,17 +16,28 @@ int main(int argc, char *argv[])
     }
     // Open the ferosonic image file specified in the command line arguments (NOTES).
     char *file argv[1];
-    File *new_file = fopen(file, "r");
+    File *raw_file = fopen(file, "r");
     if(new_file == NULL)
     {
-        // Print 
+        // Print an error messege to standard error if the forensic image file cannot be opened (NOTES)
+        printf("Could not open %s.\n", file);
+        return 1;
     }
     // Initilize variable.
-    // Flag to keep track of weather a JPEG has been found.
-    // Counter of the number of JPEGs found.
-    // Buffer to store a block of data from the forensic image(Notes)
-    // Array to store the file name of the current JPEG
-    // Pointer to the current JPEG File.
+    bool found_jpg = false; // Flag to keep track of weather a JPEG has been found.
+    int jpg_count = 0;     // Counter of the number of JPEGs found.
+    unit8_t buffer[BLOC_SIZE];  // Buffer to store a block of data from the forensic image(Notes)
+    char jpg_name[8];  // Array to store the file name of the current JPEG
+    File *outptr = NULL;  // Pointer to the current JPEG File.
 
+    // Read the forensic image file block by block (NOTES)
+    while(fread(buffer, BLOCK_SIZEN, 1, raw_file))
+    {
+        // Check if this block marks the start of a new JPEG (NOTES).
+        if(buffer[0] == oxff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            // Close 
+        }
+    }
 
 }
