0384e783f8d806efc7560aea0cf95198240301bd
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-21 10:08:23 +0000

    Thu, Sep 21, 2023, 3:08 AM -07:00

--- recover/recover.c
+++ recover/recover.c
@@ -26,17 +26,17 @@ int main(int argc, char *argv[])
     }
 
     // Initilize variable.
-    bool found_jpg = false;    // Flag to keep track of weather a JPEG has been found.
-    int jpg_count = 0;         // Counter of the number of JPEGs found.
+    bool found_jpg = false;     // Flag to keep track of weather a JPEG has been found.
+    int jpg_count = 0;          // Counter of the number of JPEGs found.
     uint8_t buffer[BLOCK_SIZE]; // Buffer to store a block of data from the forensic image(Notes)
-    char jpg_name[8];          // Array to store the file name of the current JPEG
-    FILE *outptr = NULL;       // Pointer to the current JPEG File.
+    char jpg_name[8];           // Array to store the file name of the current JPEG
+    FILE *outptr = NULL;        // Pointer to the current JPEG File.
 
     // Read the forensic image file block by block (NOTES)
     while (fread(buffer, BLOCK_SIZE, 1, raw_file))
     {
         // Check if this block marks the start of a new JPEG (NOTES).
-        if (buffer[0] == oxff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             // Close the previous jpeg file, if one was open
             if (found_jpg)
@@ -67,11 +67,11 @@ int main(int argc, char *argv[])
         }
     }
 
-        // Close the forensic image file and the last JPEG file, if one was open.
-        fclose(raw_file);
-        if (found_jpg)
-        {
-            fclose(outptr);
-        }
-        return 0;
+    // Close the forensic image file and the last JPEG file, if one was open.
+    fclose(raw_file);
+    if (found_jpg)
+    {
+        fclose(outptr);
+    }
+    return 0;
 }
