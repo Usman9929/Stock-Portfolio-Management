ad4e959ae701fadc8f5fd0f1d9f41d07a0a1d6f2
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-22 18:48:14 +0000

    Fri, Sep 22, 2023, 11:48 AM -07:00

--- hello.py
+++ hello.py
@@ -1,14 +1,4 @@
-def main():
-    height = get_height()
-    for i in range(height):
-        print("#")
+scores = [22, 44, 55]
 
-
-def get_height():
-    while True:
-        n = int(input("Height: "))
-        if n > 0:
-            break
-    return n
-
-main()
+average = sum(scores) / len(scores)
+print("Average:",average)
