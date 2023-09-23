cb8d70e67f45b6859c6ef4b539155d5d74fcabca
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-23 17:32:44 +0000

    Sat, Sep 23, 2023, 10:32 AM -07:00

--- hello.py
+++ hello.py
@@ -1,35 +1,5 @@
-text = "In the great green room"
+book = dict()
 
-words = text.split()
-
-# Roound 1
-print("Round 1")
-for word in words:
-    print(word)
-print()
-
-# Round 2
-print("Round 2")
-for word in words:
-    for c in word:
-        print(c)
-print()
-
-# Round 3
-print("Round 3")
-for word in words:
-    if "g" in word:
-        print(word)
-print()
-
-# Round 4
-print("Round 4")
-for word in words[2:]:
-    print(word)
-print()
-
-# Round 5
-print("Round 5")
-for _ in words:
-    print("Goodnight Moon")
-print()
+book["title"] = "Corduroy"
+book["author"] = "Don Freeman"
+print(book["title"], book["author"])
