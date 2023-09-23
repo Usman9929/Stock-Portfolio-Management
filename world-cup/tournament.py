04a9e678d0e90540c905ae0e80df4d8f1ba913d1
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-23 18:54:24 +0000

    Sat, Sep 23, 2023, 11:54 AM -07:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -14,12 +14,16 @@ def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python tournament.py FILENAME")
 
+        teams = []
+        filename = sys.argv[1]
+
     teams = []
     # TODO: Read teams into memory from file
     with open(filename) as file:
         file_reader = csv.DictReader(file)
         for row in file_reader:
-            teams.append(("team": row["team"] "rating": int(row["rating"] )))
+            teams.append({
+                "team": row["team"], "rating": int(row["rating"] )})
 
 
     counts = {}
