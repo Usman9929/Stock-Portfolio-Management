82a9d89dcf437495bd2112d25188236d9a1e8ffb
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-23 10:45:22 +0000

    Sat, Sep 23, 2023, 3:45 AM -07:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -65,9 +65,10 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-     while len(teams) > 1:
+    while len(teams) > 1:
         teams = simulate_round(teams)
     return teams[0]["name"]
+
 
 if __name__ == "__main__":
     main()
