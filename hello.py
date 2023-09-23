e7a776a0dcb500734b18843dee167a7bfe464e55
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-23 17:10:29 +0000

    Sat, Sep 23, 2023, 10:10 AM -07:00

--- hello.py
+++ hello.py
@@ -1,4 +1,18 @@
 text = "In the great green boy"
 
-for c in text:
-    print(c, end="")
+words = text.split()
+
+# Roound 1
+print("Round 1")
+for word in words:
+    print(word)
+print()
+
+# Round 2
+print("Round 2")
+for word in words:
+    for c in word:
+        print(c)
+print()
+
+# Rou
