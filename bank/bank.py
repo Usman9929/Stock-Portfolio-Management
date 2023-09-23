b9e8b7e7f8d35cffdb800c8256a86d07aaec5867
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-23 09:56:31 +0000

    Sat, Sep 23, 2023, 2:56 AM -07:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,3 +1,10 @@
 s = intput("Greeting: ")
 
-if s == 
+if s == "Hello":
+    print("$0")
+elif s == "Hello, Newman":
+    print("$0")
+elif s == "How you doing?":
+    print("$20")
+elif s == "What's happening?":
+    print("$100")
