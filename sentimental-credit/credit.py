6c92a18577987e9e4b3a85be0be6115c5989fbdd
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-24 07:54:40 +0000

    Sun, Sep 24, 2023, 12:54 AM -07:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,1 +1,33 @@
-# TODO
+from cs50 import get_int
+
+def main():
+    while True:
+        h = int(input("Enter credit number: "))
+        if h >0:
+            break
+
+    odd_sum_digit=0
+    even_sum_digit=0
+
+    while (h > 0):
+        last_digit= h % 10
+        odd_sum_digit += last_digit
+        even_digit= int((h / 10) % 10)
+
+        if (2 * even_digit) > 9:
+              even_digit = (2 * even_digit)-9
+        else:
+            even_digit = 2 * even_digit
+
+        even_sum_digit += even_digit
+
+        h=h//100
+
+    sum= odd_sum_digit + even_sum_digit
+    print(f"{sum}")  #Test that checksum works.
+
+
+   if (sum % 10 == 0):
+
+        if ((h >= 4000000000000 and h < 5000000000000) or (h >= 4000000000000000 and h < 5000000000000000)):
+            print("VISA")
