70a45e43685c28054ed98be7f258e5ac2bf0d29a
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-24 12:07:14 +0000

    Sun, Sep 24, 2023, 5:07 AM -07:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,48 +2,25 @@ from cs50 import get_int
 
 
 def main():
-    # Prompts user for card number
-    card = get_int("Number: ")
-
-    # If card is valid, then check the card company
-    if valid_card(card):
-        card_company(card)
-    # Card not valid
+    while True:
+        credit_card = get_int("Number: ")
+        if credit_card >= 0:
+            break # we look for the valid case here and break out
+    if chec_validity(credit_card):
+        print_card_brand(credit_card)
     else:
         print("INVALID")
 
 
-def valid_card(num):
-    # Calculates sum according to Luhn's Algorithm
-    sum = 0
-    for i, c in enumerate(reversed(str(num))):
-        if i % 2 == 0:
-            sum += int(c)
-        else:
-            for j in str(int(c) * 2):
-                sum += int(j)
-
-    # Checks if sum is divisible by 10
-    if sum % 10 == 0:
-        return True
-    else:
-        return False
 
 
-def card_company(card):
-    # Removes all the digits from card number except first 2
-    num = int(str(card)[0:2])
 
-    # Check for conditions of all companies
-    if (num is 34 or num is 37) and len(str(card)) is 15:
-        print("AMEX")
-    elif num > 50 and num < 56 and len(str(card)) is 16:
-        print("MASTERCARD")
-    elif num >= 40 and num < 50 and (len(str(card)) is 13 34235or len(str(card)) is 16):
-        print("VISA")
-    else:
-        print("INVALID")
+
+
+
+
+
 
 
 if __name__ == "__main__":
-    main()
+main()
