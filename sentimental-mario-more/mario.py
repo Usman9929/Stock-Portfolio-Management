c86e0c25a272206b2affbf7cb1aa6c254513dc00
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-24 07:41:01 +0000

    Sun, Sep 24, 2023, 12:41 AM -07:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -3,20 +3,20 @@
 while True:
     try:
         height = int(input("Height:"))
-        if (height >= 1 and height <= 8):
+        if height >= 1 and height <= 8:
             break
     except:
         print("", end="")
 
 
 for i in range(1, height + 1):
-    for j in range(1 , height + 1):
+    for j in range(1, height + 1):
         # Start printing hashes after correct number of spaces
         if j > (height - i):
             print("#", end="")
         else:
             print(" ", end="")
-    #print middle space and remaining
+    # print middle space and remaining
     if j == height:
         print(" ", "#" * i, end="")
 print()
