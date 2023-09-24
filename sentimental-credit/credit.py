8adcce03dac82a1c03d92aee9c1ed826e7d31074
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-24 12:34:19 +0000

    Sun, Sep 24, 2023, 5:34 AM -07:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -5,17 +5,17 @@ def main():
     while True:
         credit_card = get_int("Number: ")
         if credit_card >= 0:
-            break # we look for the valid case here and break out
+            break  # we look for the valid case here and break out
     if check_validity(credit_card):
         print_card_brand(credit_card)
     else:
         print("INVALID")
 
-def check_validity(cn):
 
+def check_validity(cn):
     sum = 0
     for i in range(len(str(cn))):
-        if(i % 2 == 0):
+        if i % 2 == 0:
             sum += cn % 10
         else:
             digit = 2 * (cn % 10)
@@ -25,17 +25,17 @@ def check_validity(cn):
 
     return sum % 10 == 0
 
+
 def print_card_brand(cn):
     # either can begin with a 34 or 37
     if (cn >= 34e13 and cn < 35e13) or (cn >= 37e13 and cn < 38e13):
         print("AMEX")
     elif cn >= 51e14 and cn < 56e14:
-        print("MASETCARD") # cn begin with either 51,52....56
+        print("MASETCARD")  # cn begin with either 51,52....56
     elif (cn >= 4e12 and cn < 5e12) or (cn >= 4e15 and cn < 5e15):
         print("VISA")
     else:
         print("INVALID")
-
 
 
 # Running the main fucntion
