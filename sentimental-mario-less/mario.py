81178287535c76ce11de7ebf3a09338044b3f5bc
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-24 07:06:42 +0000

    Sun, Sep 24, 2023, 12:06 AM -07:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -2,7 +2,7 @@
 height = int(input("Height:"))
 
 for i in range(height):
-    for k in range(height - i - k):
+    for k in range(height - i - 1):
         print(" ")
     for l in range(i):
         print("#")
