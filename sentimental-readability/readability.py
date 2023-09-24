da69088f787f18b704a0f0535fb5eb723ff3f06b
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-24 13:09:53 +0000

    Sun, Sep 24, 2023, 6:09 AM -07:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -17,7 +17,7 @@ num_words = len(text.split())
 num_sentences = text.count(".") + text.count("!") + text.count("?")
 
 # Calculate Average number of latters per 100 words
-L = num_latters / num_words + 100
+L = num_latters / num_words * 100
 
 # Calculate Average number of sentences per 100 wors
 S = num_sentences / num_words * 100
@@ -25,7 +25,7 @@ S = num_sentences / num_words * 100
 # Calculate Garade Level using formula
 grade_Level = 0.0588 * L - 0.296 * S - 15.8
 
-#Print Grande Level
+# Print Grande Level
 if grade_Level < 1:
     print("Before Grade 1")
 elif grade_Level >= 16:
