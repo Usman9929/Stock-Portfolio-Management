3900dd5e5a0418b10c7a79e8f8309ec3713f6091
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-09-27 09:08:50 +0000

    Wed, Sep 27, 2023, 2:08 AM -07:00

---
+++ pracitce.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main()
+{
+    int a[5] = {11, 6, 2, 9, 8};
+    for(int i = 2; i < 5; i++)
+    {
+        item = a[i];
+        j = j - 1;
+        while(item < a[j])
+        {
+            if(j == 0)
+            {
+            break;
+            }
+            else
+            if(item < a[j])
+            {
+                a[j+1] = a[j];
+            }
+            j--
+        }
+        a[j+1] = item;
+    }
+}
