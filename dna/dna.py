2b16da432be291a20307e8dba0831930be75299d
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-10-05 17:05:17 +0000

    Thu, Oct 5, 2023, 10:05 AM -07:00

--- dna/dna.py
+++ dna/dna.py
@@ -5,7 +5,7 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    if len(sys.argb) != 3:
+    if len(sys.argv) != 3:
         print("Usage: python dna.py data.csv sequence.tex")
         sys.exit(1)
     # TODO: Read database file into a variable
