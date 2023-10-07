a7ba63cf92839aae739a2dccb33b00b626c950fe
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-10-07 16:45:34 +0000

    Sat, Oct 7, 2023, 9:45 AM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -56,5 +56,12 @@ JOIN bank_accounts ba ON ba.person_id = p.id
 JOIN atm_transactions at ON at.account_number = ba.account_number
 JOIN phone_calls pc ON pc.caller = p.phone_number
 WHERE bsl.year = 2021 AND bsl.month = 7 AND bsl.day = 28 AND bsl.hour = 10 AND bsl.minute BETWEEN 15 AND 25
-AND at.atm_location = 'Leggett Street' AND at.year = 2021 AND at.month = 7 AND at.day = 28 AND at.transaction_type
+AND at.atm_location = 'Leggett Street' AND at.year = 2021 AND at.month = 7 AND at.day = 28 AND at.transaction_type = 'withdraw'
 AND pc.year = 2021 AND pc.month = 7 AND pc.day = 28 AND pc.duration < 60;
+
+--Narrow down from bruce/diana who is on flight--
+SELECT p.name
+FROM people p
+JOIN passengers ps ON p.passport_number = ps.passport_number
+WHERE ps.flight_id = 36
+AND p.name IN ('Bruce', 'Diana');
