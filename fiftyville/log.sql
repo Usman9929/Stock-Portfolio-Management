84b7f8ae4117e79f7d202ecd280dbdf67636b6e9
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-10-07 16:40:47 +0000

    Sat, Oct 7, 2023, 9:40 AM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -52,9 +52,9 @@ ORDER BY f.hour, f.minute;
 SELECT p.name
 FROM bakery_security_logs bsl
 JOIN people p ON p.license_plate = bsl.license_plate
-JOIN bank_account ba ON ba.person_id = p.id
+JOIN bank_accounts ba ON ba.person_id = p.id
 JOIN atm_transactions at ON at.account_number = ba.account_number
 JOIN phone_calls pc ON pc.caller = p.phone_number
 WHERE bsl.year = 2021 AND bsl.month = 7 AND bsl.day = 28 AND bsl.hour = 10 AND bsl.minute BETWEEN 15 AND 25
-AND at.atm_location = 'Leggett Street' AND at.year = 2021 AND at.month = 7 AND at.day = 28 AND at.trasaction_type
+AND at.atm_location = 'Leggett Street' AND at.year = 2021 AND at.month = 7 AND at.day = 28 AND at.transaction_type
 AND pc.year = 2021 AND pc.month = 7 AND pc.day = 28 AND pc.duration < 60;
