50e7bb070571012278338fe546a6f02a62691e23
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-10-07 11:16:37 +0000

    Sat, Oct 7, 2023, 4:16 AM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,5 +9,7 @@ WHERE transcript LIKE '%bakery%';
 --witness 1 truth--
 SELECT * FROM bakery_security_logs WHERE year = 2021 AND MONTH = 7 AND DAY = 28 AND HOUR = 10 AND MINUTE BETWEEN 15 AND 25;
 --check aginst license plates--
-SELECT p.nam, bsl.activity, bsl.license_plate
+SELECT p.name, bsl.activity, bsl.license_plate, bsl.year, bsl.month, bsl.day, bsl.hour, bsl.minute
+FROM bakery_security_logs bsl
+JOIN peaple p ON p.license_plate = bsl.license_plate
 WHERE bsl.year = 2021 AND bsl.month = 7 AND bsl.day = 28 and bsl.hour = 10 AND bsl.minute BETWEEN 15 AND 25;
