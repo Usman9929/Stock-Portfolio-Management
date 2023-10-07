e3be240ed25282a0d29e7ebd95b2e639e1024733
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-10-07 17:26:47 +0000

    Sat, Oct 7, 2023, 10:26 AM -07:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT p.name FROM people p
 JOIN directors d ON p.id = d.person_id
 JOIN movies m ON d.movie_id = m.id
-JOIN ra
+JOIN ratings r ON m.id = r.movie_id
 WHERE r.rating >= 9.0;
