bb8e2a9e360ee85dbbcc398942210308a9cf2968
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-10-07 11:06:35 +0000

    Sat, Oct 7, 2023, 4:06 AM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,3 +7,4 @@ SELECT * FROM interviews
 WHERE transcript LIKE '%bakery%';
 
 --witness 1 truth--
+SELECT * FROM bakery_security_logs WHERE year = 2021 AND MONTH = 7 AND DAY = 28 AND HOUR = 10 AND MINUTES BETWEEN 
