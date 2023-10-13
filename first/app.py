4921f7ff1f0aefd70fd68645faa1ae2ee5d62f50
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-10-13 17:04:18 +0000

    Fri, Oct 13, 2023, 10:04 AM -07:00

--- first/app.py
+++ first/app.py
@@ -2,13 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
-
-
-
-@app.route("/greet", methods=["POST"])
-def greet():
-    name = request.args.get("name", "world")
-    return render_template("greet.html", name=name)
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        return render_templates("greet.html", name=request.args.get("name", "world"))
