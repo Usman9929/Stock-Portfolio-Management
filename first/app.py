c67be2e89da83675682eb7a1c5f0cee56138e6e4
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-10-13 10:45:32 +0000

    Fri, Oct 13, 2023, 3:45 AM -07:00

--- first/app.py
+++ first/app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+
+@app.route("/greet")
+def greet():
+    name = request.args.get("name", "world")
+    return render_template("greet.html", name=name)
