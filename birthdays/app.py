9d850d2be497688ad0f8abc2341604ff7aaa26c6
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-10-27 19:27:45 +0000

    Fri, Oct 27, 2023, 12:27 PM -07:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -30,14 +30,14 @@ def index():
         day = request.form.get("day")
 
         #insert info into db table
-        db.execute("INSERT INTO birthday (name, month, day) VALUES (:name, :month, :day) " ,
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES (:name, :month, :day) " ,
                    name=name, month=month, day=day)
         return redirect("/")
 
     else:
 
         # TODO: Display the entries in the database on index.html
-        rows = db.execute("SELECT * FROM birthday")
+        rows = db.execute("SELECT * FROM birthdays")
 
         return render_template("index.html", rows=rows)
 
