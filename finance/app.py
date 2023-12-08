db288a41bda22df83f015a0470ba18c8c9fb1c65
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-12-08 18:52:27 +0000

    Fri, Dec 8, 2023, 10:52 AM -08:00

--- finance/app.py
+++ finance/app.py
@@ -129,7 +129,16 @@ def register():
         if password != confirmation:
             return apology("Password Do Not Match")
 
+        hash = generate_password_hash(password)
 
+        try:
+            db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
+            except:
+                return apology("Username already exists")
+
+            session["user_id"] = new_user
+
+            return redirect("/")
 
 
 @app.route("/sell", methods=["GET", "POST"])
