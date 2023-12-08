d18816b1fd15558e52aacd70d7c9a5805f67ad15
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-12-08 18:41:29 +0000

    Fri, Dec 8, 2023, 10:41 AM -08:00

--- finance/app.py
+++ finance/app.py
@@ -112,6 +112,11 @@ def register():
     if request.method == "GET":
         return render_template("register.html")
 
+    else:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
 
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
