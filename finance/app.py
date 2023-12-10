c0ebb8517af5f9dcb7e5a23bfb8e48b17eb263c1
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-12-10 10:41:50 +0000

    Sun, Dec 10, 2023, 2:41 AM -08:00

--- finance/app.py
+++ finance/app.py
@@ -97,7 +97,12 @@ def add_cash():
     if request.method == "GET":
         return render_template("add.html")
     else:
-        
+        new_cash = request.form.get("new_cash")
+
+        if not new_cash:
+            return apology("You Must Give Money")
+
+        user_id = session["user_id"]
 
 
 @app.route("/history")
