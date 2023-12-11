c66c6f923ba34b0bce63e5ef3566fdbb68fb5af1
Muhammad Usman <121403144+Usman9929@users.noreply.github.com>
2023-12-11 20:55:36 +0000

    Mon, Dec 11, 2023, 12:55 PM -08:00

--- finance/app.py
+++ finance/app.py
@@ -212,7 +212,7 @@ def register():
 def sell():
     """Sell shares of stock"""
     # Get users stocks
-    stocks = db.execute("SELECT symbol, SUM(shares) as total_shaers FROM transactions WHERE user_id = :user_id GROUP BY symbol HAVING total_shares > 0",
+    stocks = db.execute("SELECT symbol, SUM(shares) as total_shares FROM transactions WHERE user_id = :user_id GROUP BY symbol HAVING total_shares > 0",
                         user_id=session["user_id"])
 
     # if the user submit the form
